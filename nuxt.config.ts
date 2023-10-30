fa036dc866b1b12b87030b282d43a45e53afac24
jovyllebermudez <jovyllebermudez@gmail.com>
2023-10-30 09:06:13 +0800

    making pages SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,19 +25,24 @@ export default defineNuxtConfig({
       // ],
     },
   },
-  routeRules: {
-    // Homepage pre-rendered at build time
-    "/": { prerender: true },
-    "/projects": { prerender: true },
-    // // Product page generated on-demand, revalidates in background
-    // "/products/**": { swr: 3600 },
-    // // Blog post generated on-demand once until next deploy
-    // "/blog/**": { isr: true },
-    // // Admin dashboard renders only on client-side
-    // "/admin/**": { ssr: false },
-    // // Add cors headers on API routes
-    // "/api/**": { cors: true },
-    // // Redirects legacy urls
-    // "/old-page": { redirect: "/new-page" },
-  },
+  nitro: {
+    prerender: {
+      routes: ['/','/projects','/about'],
+    }
+  }
+  // routeRules: { // not sure with this
+  //   // Homepage pre-rendered at build time
+  //   "/": { prerender: true },
+  //   "/projects": { prerender: true },
+  //   // // Product page generated on-demand, revalidates in background
+  //   // "/products/**": { swr: 3600 },
+  //   // // Blog post generated on-demand once until next deploy
+  //   // "/blog/**": { isr: true },
+  //   // // Admin dashboard renders only on client-side
+  //   // "/admin/**": { ssr: false },
+  //   // // Add cors headers on API routes
+  //   // "/api/**": { cors: true },
+  //   // // Redirects legacy urls
+  //   // "/old-page": { redirect: "/new-page" },
+  // },
 })
