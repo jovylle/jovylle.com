d9a2baa66d28be2263db5566c02fefe49807def0
jovyllebermudez <jovyllebermudez@gmail.com>
2023-10-30 08:32:04 +0800

    import boxicons only to needed, hayy

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,18 @@
+<script setup>
+useHead({
+  link: [{ rel: 'stylesheet', href: "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" }]
+})
+</script>
 <template>
   <div class="container mx-auto py-10 sm:py-20">
     <ContentDoc>
       <template #not-found>
         <div class="min-h-[40vh] flex flex-col justify-center items-center">
-          <h2 class="block">
-            404 Page Not Found.
-          </h2>
+          <h2 class="block">404 Page Not Found.</h2>
           <NuxtLink class="" to="/">
-            <button class="underline underline-offset-8">Go back home. cd ~</button>
+            <button class="underline underline-offset-8">
+              Go back home. cd ~
+            </button>
           </NuxtLink>
         </div>
       </template>
