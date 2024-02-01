61ea7890fd281de31887b78fe76485817626ed1f
jovylle <jovyllebermudez@gmail.com>
2024-02-01 12:40:40 +0800

    add lofi

---
+++ pages/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <div class="container mx-auto">
+      <div class="text-center py-14">
+        <div class="text-4xl my-6 font-bold">
+          Hey there. I'm Jovylle Bermudez
+        </div>
+        <div class="text-xl my-8">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, omnis! Atque consequatur aliquid excepturi illo quia, possimus explicabo animi eos error nihil nisi accusantium repudiandae obcaecati magnam rerum? Quam, dicta.
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
