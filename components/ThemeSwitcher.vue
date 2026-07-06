f303ced3e8c857b0f8525783c2c206db65b06e3f
Jovylle <jovyllebermudez@gmail.com>
2026-07-07 05:25:42 +0800

    Revert "fix: theme dropdown opens upward instead of downward"
    This reverts commit 0dbea34937c88500bb84be2516331927eaeae95d.

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -80,9 +80,9 @@ if (import.meta.client) {
 
 .theme-switcher__dropdown {
   position: absolute;
-  bottom: 100%;
+  top: 100%;
   right: 0;
-  margin-bottom: 4px;
+  margin-top: 4px;
   background: var(--theme-dropdown-bg, #fff);
   border: 1px solid var(--theme-dropdown-border, #e5e7eb);
   border-radius: 8px;
